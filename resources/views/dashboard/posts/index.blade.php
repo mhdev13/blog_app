a787ea25d587be37bb120b97099e34d6a0871747
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-10-10 09:17:14 +0700

    update and delete post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -30,8 +30,12 @@
             <td>{{ $post->category->name  }}</td>
             <td>
                 <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-                <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-                <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                  @method('delete')
+                  @csrf
+                  <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure ?')"><span data-feather="x-circle"></span></button>
+                </form>
             </td>
           </tr>
           @endforeach
