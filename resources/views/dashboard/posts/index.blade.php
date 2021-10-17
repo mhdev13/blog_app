9e18f65bd09e006c934331962bf7161ddad6e3a4
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-10-17 17:30:18 +0700

    styling button post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -29,12 +29,16 @@
             <td>{{ $post->title }}</td>
             <td>{{ $post->category->name  }}</td>
             <td>
-                <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+                <a href="/dashboard/posts/{{ $post->slug }}" button type="button" class="btn btn-info mb-3"><span data-feather="eye"></span></button>
+                </a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" button type="button" class="btn btn-warning mb-3"><span data-feather="edit"></span></button>
+                </a>
                 <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
                   @method('delete')
                   @csrf
-                  <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure ?')"><span data-feather="x-circle"></span></button>
+                  <button class="btn btn-danger mb-3" onclick="return confirm('Are You Sure ?')">
+                    <span data-feather="x-circle"></span>
+                  </button>
                 </form>
             </td>
           </tr>
