5c6872824900ce1a0a71cec54464a6195f01b68b
DESKTOP-QUOH02S\M. Hidayaturrohman <mhidayaturachman@gmail.com>
2021-11-27 15:57:18 +0700

    fixing foreache

--- resources/views/portofolio.blade.php
+++ resources/views/portofolio.blade.php
@@ -7,33 +7,34 @@
       <div class="header text-center">
         <h1>My Portofolio</h1>
       </div>
-      @if ($portofolio)
-        @foreach ($portofolio as $p)
+     
           <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-3">
-            <div class="col">
-              <div class="card shadow-sm">
-                <div class="card-body text-center">
-                  <b>{{ $p['title'] }}</b>
-                  <hr>
-                  <p><a href='{{ $p['url'] }}' target="_blank" class="link-dark ms-1">{{ $p['url'] }}</a></p>
-                </div>
-                <div class="card-body">
-                  <img src="{{ asset('storage/'. $p['image']) }}" class="img-fluid" alt="">
-                </div>
-                <div class="card-body">
-                  <p class="card-text">{{ $p['description'] }}</p>
-                  <hr>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <span class="badge bg-primary">{{ $p['category'] }}</span>
+            @if ($portofolio)
+              @foreach ($portofolio as $p)
+              <div class="col">
+                <div class="card shadow-sm">
+                  <div class="card-body text-center">
+                    <b>{{ $p['title'] }}</b>
+                    <hr>
+                    <p><a href='{{ $p['url'] }}' target="_blank" class="link-dark ms-1">{{ $p['url'] }}</a></p>
+                  </div>
+                  <div class="card-body">
+                    <img src="{{ asset('storage/'. $p['image']) }}" class="img-fluid" alt="">
+                  </div>
+                  <div class="card-body">
+                    <p class="card-text">{{ $p['description'] }}</p>
+                    <hr>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <span class="badge bg-primary">{{ $p['category'] }}</span>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+              @endforeach
+            @endif
           </div>
-        @endforeach
-      @endif
     </div>
   </div
 
